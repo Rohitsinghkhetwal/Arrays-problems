b68f8dc2710415d83be0cd89af97ba73650e45d2
rohit <raj0eshsing0909@gmail.com>
2024-03-31 23:35:29 +0530

    optimal solution of intersection of two arrays

--- IntersectionofSorted.cpp
+++ IntersectionofSorted.cpp
@@ -21,11 +21,36 @@ vector<int>Intersection(vector<int>a, vector<int>b){
     return ans;
 }
 // time complexity of this algorithm will be O(n1 X n2) O(n2)
+
+//moving onto optimal solution 
+
+vector<int>OptimalSol(vector<int>a, vector<int>b){
+    int n1 = a.size();
+    int n2 = b.size();
+    int i = 0;
+    int j = 0;
+    vector<int>result;
+    while(i < n1 && j < n2){
+       if(a[i] < b[j]){
+        i++;
+
+       }else if(a[i] > b[j]){
+        j++;
+       }else{
+        result.push_back(a[i]);
+        i++;
+        j++;
+       }
+    }
+    return result;
+}
+
+// time comlexity of optimal approach will be the O(n1 + n2) wrost case 
 int main(){
     cout << " Intersection of two arrays" << endl;
     vector<int>arrOne = {1,1,2,2,3,3,4,4,5,6};
     vector<int>arrTwo = {1,1,2,2,3,3,5,5,7,8};
-    vector<int>result = Intersection(arrOne, arrTwo); 
+    vector<int>result = OptimalSol(arrOne, arrTwo); 
     for(auto i: result){
         cout << i << endl;
     }
