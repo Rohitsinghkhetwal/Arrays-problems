53e9029c8620639ac4751286539012051a7fa12f
rohit <raj0eshsing0909@gmail.com>
2024-04-09 23:31:59 +0530

    added the optimal solution of mojority element of n / 2

--- greaterThanNbyTwo.cpp
+++ greaterThanNbyTwo.cpp
@@ -32,11 +32,37 @@ int GreaterThanHalf(vector<int>arr){
     return -1;
 }
 // we will be writing the optimal approach for this problem
+//in this optimal solution we will be using moore's votion algorithm
+int GreaterThanNbyTwo(vector<int>ar){
+    int count = 0;
+    int element;
+    for(int i = 0; i < ar.size(); i++){
+        if(count == 0){
+            count++;
+            element = ar[i];
+        }else if(ar[i] == element){
+            count++;
+        }else{
+            count--;
+        }
+    }  
+    int counter = 0;
+    for(int i = 0; i < ar.size(); i++){
+        if(ar[i] == element) counter++;
+    }
+    if(counter > (ar.size()) / 2){
+        return element;
+
+    }
+
+    return -1;
+}
+
 int main(){
     cout << "return the number that is greater than n / 2" << endl;
     vector<int>arr = {2,3,4,2,2,2,5};
     // int res = GreaterThanHalf(arr);
-    int res =  GH(arr);
+    int res =  GreaterThanNbyTwo(arr);
     cout << "the result will be the " << res << endl;
     return 0;
 }
